996fa65fa1948c7415d4f1b737f9f0e16b49e67d
Zacharias Hellberg <gertrud@gertrud.gertrud-network>
2016-12-29 11:43:04 +0200

    fixed a couple of typos. logger tested and working

--- folder_listener.py
+++ folder_listener.py
@@ -1,16 +1,17 @@
 # http://brunorocha.org/python/watching-a-directory-for-file-changes-with-python.html
-
+import sys
 import time
-from watchdogs.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
 if __name__ == "__main__":
-  ligging.basicConfig(level = logging.INFO,format='%(asctime)s - %(message)s',datefmt='%H:%M:%S %d.%m.%Y')
+  logging.basicConfig(level = logging.INFO,format='%(asctime)s - %(message)s',datefmt='%H:%M:%S %d.%m.%Y')
   path = sys.argv[1] if len(sys.argv) > 1 else '.'
   event_handler = LoggingEventHandler()
   observer = Observer()
-  observer.schedule(event_handler, path, recurseive = True)
-  ovserver.start()
+  observer.schedule(event_handler, path, recursive = True)
+  observer.start()
   try:
       while True:
           time.sleep(1)
