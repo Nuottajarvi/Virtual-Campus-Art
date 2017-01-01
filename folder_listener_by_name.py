41c1f6c676853934f1e965fe41d97f47e3f1ebd4
Zacharias Hellberg <gertrud@gertrud.gertrud-network>
2017-01-01 19:52:17 +0200

    commented and tidied up the code

--- folder_listener_by_name.py
+++ folder_listener_by_name.py
@@ -5,40 +5,38 @@
 import logging
 import sys
 import time
-
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
 import re
 import requests
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 logging.basicConfig(level=logging.ERROR)
 
 class MyEventHandler(FileSystemEventHandler):
     def __init__(self, observer):
         self.observer = observer
-
+    #Triggers when a new file is made. Ignores directories.
     def on_created(self, event):
         try:
             eventStr = str(event)
+            #Extracts the filename from the "file created"-event.
             filename = re.search('src_path=\'(.+?)\'', eventStr).group(1)
             print ("filename: ", filename)
+            #Opens the new file and makes a post request to given url.
             with open(filename, 'rb') as f:
                 r = requests.post('http://foo.bar/foobar, files={filename: f})
-
-
         except AttributeError:
             pass
-
         if not event.is_directory:
             print ("file created")
 
 def main(argv=None):
-
+    #Define path to the folder to be watched.
     path = '.'
-
     observer = Observer()
     event_handler = MyEventHandler(observer)
 
+    #Apply Myeventhander on given folder either recursively or not.
     observer.schedule(event_handler, path, recursive=False)
     observer.start()
     try:
